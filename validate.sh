810310c836f2dceea126123f5a68d319a4848fc4
Kevin Dreßler <kvndrsslr@gmail.com>
2018-05-03 10:22:08 +0200

    updated vas, vds, common, addded user

--- validate.sh
+++ validate.sh
@@ -5,7 +5,7 @@ Options:
 " && exit
 [[ $1 == "--install" ]] && npm install -g http-server && [[ $2 == "" ]] && exit
 http-server&
-sleep 2
+sleep 4
 docker network create -d bridge --subnet 192.168.0.0/24 --gateway 192.168.0.1 oasnet
 docker run -it --rm --net=oasnet usabillabv/openapi3-validator "http://192.168.0.1:8080/${1}"
 docker network rm oasnet
